12c0a68d4c2c9090b737c4d5ba2724a436291cdd
EethanF <ethan_frediani@yahoo.co.uk>
2022-03-16 10:15:38 +0000

    restAPI

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 require("./db/connection");
 const express = require("express");
+const cors = require("cors"); //order important!
 const movieRouter = require("./movie/movieRoutes");
+const userRouter = require("./user/userRoutes");
 const app = express();
 const port = 5000;
 
 app.use(express.json()); //order important!
+app.use(cors());
 app.use(movieRouter);
+app.use(userRouter );
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
